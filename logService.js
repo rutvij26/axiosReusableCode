08ab8f982a2801798daaabb6750a2a270abdc252
Rutvij Sathe <44632403+rutvij26@users.noreply.github.com>
2021-01-30 18:37:13 +0530

    LoggingService_Sentry

---
+++ logService.js
@@ -0,0 +1,23 @@
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
+
+function init() {
+  Sentry.init({
+    dsn:
+      "https://1e84d3d2f7f945e9983b49a9e2dcde32@o513559.ingest.sentry.io/5615755",
+    integrations: [new Integrations.BrowserTracing()],
+
+    // We recommend adjusting this value in production, or using tracesSampler
+    // for finer control
+    tracesSampleRate: 1.0,
+  });
+}
+
+function log(error) {
+  Sentry.captureException(error);
+}
+
+export default {
+  init,
+  log,
+};
